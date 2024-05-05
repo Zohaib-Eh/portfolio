f6cc46833380ae112ddc23829687bbd531a18b8e
Zohaib <zohaib.ehtesham@gmail.com>
2024-05-05 02:25:31 +0100

    added EmailJS for functionality

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { Row, Col, Container } from "react-bootstrap";
 import contactImg from '../../assets/img/contactImg.svg'
 import './contact.styles.css'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
     const formInitialDetails = {
@@ -16,15 +17,31 @@ const Contact = () => {
     const [buttonText, setButtonText] = useState('Send');
     const [status,setStatus] = useState({});
 
+    const serviceId = 'service_bulfi79'
+    const templateId = 'template_prbe0g7'
+    const publicKey = 'xG-GbAvM_t_ULdyvQ'
+
     const onFormUpdate = (category,value) => {
         setFormDetails({
             ...formDetails,
             [category] : value
         })
     }
+    const formRef = useRef(null);
 
-    const submitHandler = () => {
-
+    const submitHandler = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(serviceId, templateId, formRef.current, publicKey)
+        .then(
+            (response) => {
+                setStatus({ success: true, message: 'Email sent successfully' });
+                console.log('Email sent successfully:', response);
+            },
+            (error) => {
+                setStatus({ success: false, message: 'Failed to send email' });
+                console.error('Email sending failed:', error);
+            }
+        );
     }
 
     return (
@@ -37,21 +54,21 @@ const Contact = () => {
                     <Col md={6}>
                         <h2>Let's Connect</h2>
                         <h4>Drop me a line and let's sync up like flawless software updates!</h4>
-                        <form onSubmit={submitHandler}>
+                        <form ref={formRef} onSubmit={(e) => submitHandler(e)}>
                             <Row>
                                 <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)}/>
+                                    <input type="text" name='firstName' value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)}/>
                                 </Col>
                                 <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                                    <input type="text" name='lastName' value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
                                 </Col>
                                 <Col sm={6} className="px-1">
-                                    <input type="email" value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)}/>
+                                    <input type="email" name='email' value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)}/>
                                 </Col>
                                 <Col sm={6} className="px-1">
-                                    <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                                    <input type="tel" name='phone' value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
                                 </Col>
-                                <textarea row='6' value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}/>
+                                <textarea row='6' name='message' value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}/>
                                 <button type="submit"><span>{buttonText}</span></button>
                                 {
                                     status.message &&
